07ef744096716f3f71f6a41b9496d75718e905d4
Alwisteins <alwi.wahyu.w@gmail.com>
2025-09-11 18:46:39 +0700

    feat(order): add address

---
+++ app/Filament/Resources/Orders/RelationManagers/AddressRelationManager.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources\Orders\RelationManagers;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class AddressRelationManager extends RelationManager
+{
+    protected static string $relationship = 'address';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('first_name')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('last_name')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('phone')
+                    ->required()
+                    ->tel()
+                    ->maxLength(25),
+
+                TextInput::make('city')
+                    ->required()
+                    ->maxLength(255),
+                
+                TextInput::make('state')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('zip_code')
+                    ->required()
+                    ->maxLength(255),
+
+                Textarea::make('street_address')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('street_address')
+            ->searchable(false)
+            ->columns([
+                TextColumn::make('full_name')
+                ->searchable(),
+
+                TextColumn::make('phone')
+                ->searchable(),
+
+                TextColumn::make('state')
+                ->searchable(),
+
+                TextColumn::make('city')
+                ->searchable(),
+
+                TextColumn::make('zip_code')
+                ->searchable(),
+
+                TextColumn::make('street_address')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
